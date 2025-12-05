a49ef7a7948b6914bfa70c423ad848ca23c3c920
YvenChauvaux <71826967+YvenChauvaux@users.noreply.github.com>
2025-12-05 16:55:33 +0100

    First parts of the frontend added

--- Frontend/finance-frontend/src/app/app.ts
+++ Frontend/finance-frontend/src/app/app.ts
@@ -1,12 +1,49 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  standalone: true,
+  templateUrl: './app.html',              
+  styleUrls: ['./app.scss'],
+  imports: [
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    NgIf,           
+    NgFor,       
+  ]
 })
-export class App {
-  protected readonly title = signal('finance-frontend');
+export class App implements OnInit {
+  budgets: any[] = [];
+  loading = true;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadBudgets();
+  }
+
+  loadBudgets() {
+    this.http.get('http://localhost:8080/api/budgets').subscribe({
+      next: (data: any) => {
+        this.budgets = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Fout bij ophalen:', err);
+        this.loading = false;
+      }
+    });
+  }
+
+  openSwagger() {
+    window.open('http://localhost:8080/swagger-ui.html', '_blank');
+  }
 }
